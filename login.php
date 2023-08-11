b14df45c68b2d5498070d855cecb3925516df61b
Ragesh <137527716+Ragesh02@users.noreply.github.com>
2023-08-11 12:42:44 +0530

    login.php

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
             exit;
         }
         else{
-            echo "Incorrect Log In";
+            header("Location: login.php?message= Email or Password Incorrect");
         }
 
         mysqli_close($connect);
@@ -52,7 +52,7 @@
     <form action="login.php" method = "POST">
         <div class="form-group">
             <label>Email</label>
-            <input type="text" name="email" class="form-control" id="email" required>
+            <input type="email" name="email" class="form-control" id="email" required>
         </div>
         <div class="form-group">
             <label>Password</label>
@@ -62,7 +62,7 @@
 
         <button class="btn btn-success" type="submit" id="login">Login</button>
         <a href="register.php" class="btn btn-warning">Cancel</a>
-        <a href="register.php" class="btn btn-primary">Click to Register</a>
+        <a href="register.php" class="btn btn-primary">New Register</a>
 
         
     </form>
